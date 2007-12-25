57276db87732c78f3612d84ed68f94352cf423f5
Ola Bini <ola.bini@gmail.com>
2007-12-25 13:16:12 +0000

    Add a failing test and update jruby version

--- test/unit/unit_test.rb
+++ test/unit/unit_test.rb
@@ -7,6 +7,11 @@ unit_tests do
   test "that a file in a lib directory gets loaded" do 
     assert $has_loaded_lib_file
   end
+
+  test "that the class loader used for Java classes under test is the JRuby class loader" do 
+    $stderr.puts org.jtestr.RuntimeFactory.java_class.class_loader
+    assert false
+  end
   
 #  test "exception" do 
 #    1.should == 2
