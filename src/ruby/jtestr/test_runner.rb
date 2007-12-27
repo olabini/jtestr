4b92e0c06473a05bab4e340eee00cc8af97c03aa
Ola Bini <ola.bini@gmail.com>
2007-12-27 01:45:00 +0000

    Bit better reporting

--- src/ruby/jtestr/test_runner.rb
+++ src/ruby/jtestr/test_runner.rb
@@ -6,6 +6,7 @@ module JtestR
     include JUnitTestRunning
 
     def run(dirname = nil, log_level = JtestR::SimpleLogger::DEBUG, outp_level = JtestR::GenericResultHandler::QUIET, output = STDOUT)
+      output.puts "Running from #{dirname || Dir['{test,src/test,tests,src/tests}'].join(',')}"
       JtestR::logger = JtestR::SimpleLogger
       JtestR::result_handler = JtestR::GenericResultHandler
 
@@ -28,7 +29,7 @@ module JtestR
       run_tests
       
       @configuration.configuration_values(:after).flatten.each &:call
-      
+      output.puts "Finished test run: #{@result && (!@errors || @errors.empty?) ? 'SUCCESSFUL' : 'FAILED'}"
       @result && (!@errors || @errors.empty?)
     rescue Exception => e
       log.err e.inspect
@@ -54,7 +55,7 @@ module JtestR
     end
     
     def load_configuration
-      @test_directories = @dirname ? [@dirname.strip] : ["test","src/test"]
+      @test_directories = @dirname ? [@dirname.strip] : ["test","src/test", "tests", "src/tests"]
       
       @config_files = @test_directories.map {|dir| File.join(dir, "jtestr_config.rb") }.select {|file| File.exist?(file)}
       
