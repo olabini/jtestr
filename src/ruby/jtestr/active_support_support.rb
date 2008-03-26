a24ba91c535505f2c5b985fc1239062ff4299c6d
Ola Bini <ola.bini@gmail.com>
2008-03-26 17:51:07 +0000

    Temporarily disable activesupport

--- src/ruby/jtestr/active_support_support.rb
+++ src/ruby/jtestr/active_support_support.rb
@@ -1,3 +1,3 @@
-$:.unshift File.expand_path(File.join(File.dirname(__FILE__), '..', 'activesupport', 'lib'))
+# $:.unshift File.expand_path(File.join(File.dirname(__FILE__), '..', 'activesupport', 'lib'))
 
-require 'active_support'
+#require 'active_support'
