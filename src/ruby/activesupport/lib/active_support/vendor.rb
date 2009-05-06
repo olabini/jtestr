856719594d79d300589532aeaf36fa1e1630246e
Ola Bini <ola.bini@gmail.com>
2009-05-06 20:25:35 +0000

    Update to the new active support version

--- src/ruby/activesupport/lib/active_support/vendor.rb
+++ src/ruby/activesupport/lib/active_support/vendor.rb
@@ -1,5 +1,12 @@
-$:.unshift "#{File.dirname(__FILE__)}/vendor/builder-2.1.2"
-$:.unshift "#{File.dirname(__FILE__)}/vendor/xml-simple-1.0.11"
-$:.unshift "#{File.dirname(__FILE__)}/vendor/memcache-client-1.5.0"
-$:.unshift "#{File.dirname(__FILE__)}/vendor/tzinfo-0.3.8"
+# Prefer gems to the bundled libs.
+require 'rubygems'
 
+$:.unshift "#{File.dirname(__FILE__)}/vendor/builder-2.1.2"
+require 'builder'
+
+$:.unshift "#{File.dirname(__FILE__)}/vendor/memcache-client-1.6.5"
+
+$:.unshift "#{File.dirname(__FILE__)}/vendor/tzinfo-0.3.12"
+
+$:.unshift "#{File.dirname(__FILE__)}/vendor/i18n-0.1.3/lib"
+require 'i18n'
