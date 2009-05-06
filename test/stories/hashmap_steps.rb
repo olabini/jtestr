09e65af4b00261f49ba89c8d982365e3afae579f
Ola Bini <ola.bini@gmail.com>
2009-05-06 20:26:00 +0000

    Handle the hashmap a bit differently

--- test/stories/hashmap_steps.rb
+++ test/stories/hashmap_steps.rb
@@ -1,14 +1,12 @@
-import java.util.HashMap
-
 $maps = { }
 
 steps_for(:hashmap) do
   Given('my $map_name contains "$key" with value "$value"') do |map_name, key, value|
-    $maps[map_name] ||= HashMap.new
+    $maps[map_name] ||= java.util.HashMap.new
     $maps[map_name][key] = value
   end
   Given('my $map_name is empty') do |map_name|
-    $maps[map_name] ||= HashMap.new
+    $maps[map_name] ||= java.util.HashMap.new
     $maps[map_name].clear
   end
   When('I transfer value with key "$key" from my $from_map to my $to_map') do |key, from_map, to_map|
